f77efc4bacf54911d29ed3a1664abba99b832d72
Raynos <raynos2@gmail.com>
2014-05-04 11:24:18 -0700

    wip

--- bin/example-tasks.js
+++ bin/example-tasks.js
@@ -7,6 +7,7 @@ var browserifyEditor = require('./browserify-editor')
 var examplesDir = path.join(__dirname, '..', 'examples')
 var examplesTasks = [
     browserifyTask('geometry'),
+    browserifyTask('github-issues-viewer'),
     browserifyTask('todomvc'),
     browserifyEditorTask('field-reset'),
     browserifyEditorTask('bmi-counter'),
